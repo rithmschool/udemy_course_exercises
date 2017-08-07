fcb12ab7f3a97dd1cdff8085b4082c161d2f356e
Elie Schoppik <elie@users.noreply.github.com>
2017-08-07 11:37:12 -0700

    Update call-apply-bind-exercises.js

--- closures-and-keyword-this/call-apply-bind/call-apply-bind-exercises.js
+++ closures-and-keyword-this/call-apply-bind/call-apply-bind-exercises.js
@@ -1,5 +1,3 @@
-// https://github.com/rithmschool/intermediate_javascript_exercises/blob/master/call_apply_bind_exercise/callApplyBindSpec.js
-
 function arrayFrom(arrayLikeObject){
     return [].slice.call(arrayLikeObject)
 }
@@ -12,25 +10,6 @@ function sumEvenArguments(){
         }
         return acc;
     },0)
-}
-
-// Write a function called sumEvenArguments which takes all of the arguments passed to a function and returns the sum of the even ones.
-
-// sumEvenArguments(1,2,3,4) // 6
-// sumEvenArguments(1,2,6) // 8
-// sumEvenArguments(1,2) // 2
-
-// // Write a function called invokeMax which accepts a function and a maximum amount. invokeMax should return a function that when called increments a counter. If the counter is greater than the maximum amount, the inner function should return "Maxed Out"
-
-// var addOnlyThreeTimes = invokeMax(add,3);
-// addOnlyThreeTimes(1,2) // 3
-// addOnlyThreeTimes(2,2) // 4
-// addOnlyThreeTimes(1,2) // 3
-// addOnlyThreeTimes(1,2) // "Maxed Out!"
-
-
-function add(a,b){
-    return a+b
 }
 
 function invokeMax(fn, num){
@@ -52,22 +31,20 @@ function once(fn, thisArg){
     }
 }
 
-function bind(fn, thisArg){
-    var outerArgs = [].slice.call(arguments,2)
-    return function(){
-        var innerArgs = [].slice.call(arguments)
-        var allArgs = outerArgs.concat(innerArgs)
-        return fn.apply(thisArg, allArgs)
-    }
-}
-
-
-// BONUS!
 function flip(fn, thisArg){
     var outerArgs = [].slice.call(arguments,2)
     return function(){
         var innerArgs = [].slice.call(arguments)
         var allArgs = outerArgs.concat(innerArgs).slice(0, fn.length)
         return fn.apply(thisArg, allArgs.reverse())
+    }
+}
+
+function bind(fn, thisArg){
+    var outerArgs = [].slice.call(arguments,2)
+    return function(){
+        var innerArgs = [].slice.call(arguments)
+        var allArgs = outerArgs.concat(innerArgs)
+        return fn.apply(thisArg, allArgs)
     }
 }
