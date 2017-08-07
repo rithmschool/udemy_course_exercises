6e3a5d2712a5efab90ca6f359d2c45e309216782
Elie Schoppik <elie@users.noreply.github.com>
2017-08-07 11:38:47 -0700

    Update closures-exercises.js

--- closures-and-keyword-this/closures/closures-exercises.js
+++ closures-and-keyword-this/closures/closures-exercises.js
@@ -1,9 +1,3 @@
-// Write a function called specialMultiply which accepts two parameters. If the function is passed both parameters, it should return the product of the two. If the function is only passed one parameter - it should return a function which can later be passed another parameter to return the product. You will have to use closure and arguments to solve this.
-
-specialMultiply(3,4); // 12
-specialMultiply(3)(4); // 12
-specialMultiply(3); // returns a function
-
 function specialMultiply(a,b){
   if(arguments.length === 1){
     return function(b){
@@ -12,12 +6,6 @@ function specialMultiply(a,b){
   }
   return a*b;
 }
-
-// Write a function called guessingGame which takes in one parameter amount. The function should return another function that takes in a parameter called guess. In the outer function, you should create a variable called answer which is the result of a random number between 0 and 10 as well as a variable called guesses which should be set to 0.
-
-// In the inner function, if the guess passed in is the same as the random number (defined in the outer function) - you should return the string "You got it!". If the guess is too high return "Your guess is too high!" and if it is too low, return "Your guess is too low!". You should stop the user from guessing if the amount of guesses they have made is greater than the initial amount passed to the outer function.
-
-// You will have to make use of closure to solve this problem.
 
 function guessingGame(amount){
     var answer = Math.floor(Math.random()*11);
@@ -40,16 +28,3 @@ function guessingGame(amount){
         return "All done playing!"
     }
 }
-
-var game = guessingGame(5)
-game(1) // "You're too low!"
-game(8) // "You're too high!"
-game(5) // "You're too low!"
-game(7) // "You got it!"
-game(1) // "You are all done playing!"
-
-var game2 = guessingGame(3)
-game2(5) // "You're too low!"
-game2(3) // "You're too low!"
-game2(1) // "No more guesses the answer was 0"
-game2(1) // "You are all done playing!"
