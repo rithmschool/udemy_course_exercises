125a6b4eb5ea8d28a0ad9271d42e97e2350e79ca
Benjamin Liden <benjamin.c.liden@gmail.com>
2017-10-14 15:57:59 -0400

    Fixed array returning uppercase letters

--- advanced-array-methods/forEach/forEach-exercises.js
+++ advanced-array-methods/forEach/forEach-exercises.js
@@ -1,51 +1,51 @@
 // forEach
 
-function doubleValues(arr){
-    var newArr = [];
-    arr.forEach(function(val){
-        newArr.push(val * 2)
-    })
-    return newArr;
+function doubleValues(arr) {
+   var newArr = [];
+   arr.forEach(function(val) {
+      newArr.push(val * 2)
+   })
+   return newArr;
 }
 
-function onlyEvenValues(arr){
-    var newArr = [];
-    arr.forEach(function(val){
-        if(val % 2 === 0){
-            newArr.push(val)
-        }
-    })
-    return newArr;
+function onlyEvenValues(arr) {
+   var newArr = [];
+   arr.forEach(function(val) {
+      if (val % 2 === 0) {
+         newArr.push(val)
+      }
+   })
+   return newArr;
 }
 
-function showFirstAndLast(arr){
-    var newArr = [];
-    arr.forEach(function(val){
-        newArr.push(val[0] + val[val.length-1])
-    });
-    return newArr;
+function showFirstAndLast(arr) {
+   var newArr = [];
+   arr.forEach(function(val) {
+      newArr.push(val[0] + val[val.length - 1])
+   });
+   return newArr;
 }
 
-function addKeyAndValue(arr,key,value){
-    arr.forEach(function(val){
-        val[key] = value;
-    });
-    return arr;
+function addKeyAndValue(arr, key, value) {
+   arr.forEach(function(val) {
+      val[key] = value;
+   });
+   return arr;
 }
 
-function vowelCount(str){
-    var splitArr = str.split("");
-    var obj = {};
-    var vowels = "aeiou";
+function vowelCount(str) {
+   var splitArr = str.toLowerCase().split("");
+   var obj = {};
+   var vowels = "aeiou";
 
-    splitArr.forEach(function(letter){
-        if(vowels.indexOf(letter.toLowerCase()) !== -1){
-            if(obj[letter]){
-                obj[letter]++;
-            } else{
-                obj[letter] = 1;
-            }
-        }
-    });
-    return obj;
+   splitArr.forEach(function(letter) {
+      if (vowels.indexOf(letter) !== -1) {
+         if (obj[letter]) {
+            obj[letter]++;
+         } else {
+            obj[letter] = 1;
+         }
+      }
+   });
+   return obj;
 }
