882cc95fb678736d11218df2e4fd1facf23bd625
Sean C. Mangosing <seanmangosing@gmail.com>
2017-12-11 22:46:20 -0800

    Completed Exercises

--- object-oriented-programming/constructors/constructors-exercises.js
+++ object-oriented-programming/constructors/constructors-exercises.js
@@ -1,5 +1,31 @@
 // Create a constructor function for a Person, each person should have a firstName, lastName, favoriteColor and favoriteNumber.
+function Person(firstName, lastName, favoriteColor, favoriteNumber) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.favoriteColor = favoriteColor;
+  this.favoriteNumber = favoriteNumber;
 
 // Write a method called multiplyFavoriteNumber that takes in a number and returns the product of the number and the Person's favorite number
-
+function Person(firstName, lastName, favoriteColor, favoriteNumber) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.favoriteColor = favoriteColor;
+  this.favoriteNumber = favoriteNumber;
+  this.multiplyFavoriteNumber = function multiplyFavoriteNumber(num) {
+    return num * this.favoriteNumber;
+  }
+}
 // refactor
+
+
+function Parent(firstName, lastName, favoriteColor, favoriteFood) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.favoriteColor = favoriteColor;
+  this.favoriteFood = favoriteFood;
+}
+
+function Child(firstName, lastName, favoriteColor, favoriteFood) {
+  Parent.call(this, firstName, lastName, favoriteColor)
+  this.favoriteFood = favoriteFood;
+}
