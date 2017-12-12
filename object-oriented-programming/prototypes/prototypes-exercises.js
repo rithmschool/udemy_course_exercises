88e2c1c9c7665ed6163315554beb09b4b7c9e103
Sean C. Mangosing <seanmangosing@gmail.com>
2017-12-11 23:53:12 -0800

    Completed Exercises

--- object-oriented-programming/prototypes/prototypes-exercises.js
+++ object-oriented-programming/prototypes/prototypes-exercises.js
@@ -6,11 +6,20 @@
 
 // Add a function on the Person.prototype called addToFamily which adds an object constructed from the Person constructor to the family array. To make sure that the object you are adding is an object construced from the Person constructor - take a look at the instanceofoperator. Make sure that your family array does not include duplicates! This method should the length of the family array.
 
-function Person(firstName, lastName, favoriteColor, favoriteNumber) {}
+function Person(firstName, lastName, favoriteColor, favoriteNumber) {
 
-Person.prototype.fullName = function() {};
+}
 
-Person.prototype.addToFamily = function(person) {};
+Person.prototype.fullName = function () {
+  return this.firstName + " " + this.lastName;
+};
+
+Person.prototype.addToFamily = function (person) {
+  if (this.family.indexOf(person) === -1 && person instanceof Person) {
+    this.family.push(person)
+  }
+  this.family.length;
+};
 
 // Part II:
 
@@ -18,8 +27,20 @@ Person.prototype.addToFamily = function(person) {};
 
 // Implement your own version of Array.prototype.map
 
-Array.prototype.map = function(callback) {};
+Array.prototype.map = function(callback) {
+  var newArr = [];
+  for (var i = 0; i < this.length; i++) {
+    newArr.push(callback(this[i], i, this))
+  }
+  return newArr;
+};
 
 // Implement a function that reverses a string and place it on the String.prototype
 
-String.prototype.reverse = function() {};
+String.prototype.reverse = function() {
+  let nStr = '';
+  for (var i = this.length - 1; i >= 0; i--) {
+    nStr += this[i]
+  }
+  return nStr;
+};
